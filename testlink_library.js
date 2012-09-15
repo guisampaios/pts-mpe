8aac4cf26b5563aaf67552dfde4e3a077534ee5b
guisampaios <guisampaios@gmail.com>
2012-09-15 12:19:31 -0300

    Atualizar15092012
    Nome Diretórios.
    
    // Function open_bug_add_windows(exec_id, tproject_id, testcase_id) :
        -- Abre a pop up de reportar Bug no MantisBT, com o código do teste que foi executado(exec_id),
    com o código do projeto da sessão do testlink(tproject_id) e o código do caso de teste(testcase_id).
    
    // Function create_project_mantisBT(project_id)
        -- Abre a pop up de Criar Projeto no MantisBT, com o código do projeto da sessão do testlink(project_id)

--- testlink_library.js
+++ testlink_library.js
@@ -588,12 +588,12 @@ function tree_getCheckBox(id)
 
 function open_bug_add_window(exec_id,tproject_id,testcase_id)
 {
-	window.open("http://localhost/Projeto%20Mestrado/mantisbt-1.2.11/bug_report_page_testlink.php?exec_id="+exec_id+"&tproject_id="+tproject_id+"&testcase_id="+testcase_id,"bug_add",
+	window.open("http://localhost/bug_report_page_testlink.php?exec_id="+exec_id+"&tproject_id="+tproject_id+"&testcase_id="+testcase_id,"bug_add",
 	            "width=830,height=650,resizable=yes,dependent=yes");
 }
 function create_project_mantisBT(tproject_id)
 {
-    window.open("http://localhost/Projeto%20Mestrado/mantisbt-1.2.11/integracao/createProjectMantis.php?tproject_id="+tproject_id,"create_project_mantisBT","width=870,height=650,resizable=yes,dependent=yes");
+    window.open("http://localhost/mantisbt/integracao/createProjectMantis.php?tproject_id="+tproject_id,"create_project_mantisBT","width=870,height=650,resizable=yes,dependent=yes");
 }
 function bug_dialog()
 {
